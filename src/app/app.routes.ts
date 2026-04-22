00a3da41c4dd5e38f77599a7d20d572c78317d00
David G <davidsantiagoguerrerotolosa@gmail.com>
2026-04-22 14:14:12 -0500

    Acabar Presentacion 2
    imprime pero toca gregar cosas a mongo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { AnimalComponent } from './components/animal-component/animal-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: AnimalComponent }
+];
